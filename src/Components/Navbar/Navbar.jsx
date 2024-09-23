d562b50f0e0f38364421b30d357e437da966346f
Akash Kumar <kumarakash91384@gmail.com>
2024-09-23 12:43:00 +0530

    Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ import profile_icon from '../../Components/Assets/user.png'
 import admin_logo from '../../AdminPanel/Components/assets/goku.gif';
 import { Link, useNavigate } from 'react-router-dom';
 import ProgressBar from '../ProgressBar/ProgressBar';
+import { jwtDecode } from 'jwt-decode';
 import { ShopContext } from '../../Context/ShopContext';
 import { Tooltip, Offcanvas } from 'bootstrap';
 import useIntersectionObserver from '../Hooks/Hooks';
@@ -131,6 +132,7 @@ const Navbar = (props) => {
 
     return (
         <>
+            <ProgressBar />
             <nav className={`fixed navbar container-fluid navbar-expand-lg navbar-${props.mode} bgColor-${props.mode}`}>
                 <div className={`container-fluid navbar-${props.mode} bgColor-${props.mode}`}>
                     <Link to="/">
