30c9169e45cf8d0ef581ef3993e83f8cb820fcb1
Akash Kumar <kumarakash91384@gmail.com>
2024-09-29 08:44:42 +0530

    adde skeletan card when data is not loaded

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
-import './Item.css'
-import { Link } from 'react-router-dom'
-import useIntersectionObserver from '../Hooks/Hooks'
+import React from 'react';
+import './Item.css';
+import { Link } from 'react-router-dom';
+import useIntersectionObserver from '../Hooks/Hooks';
+
+// Skeleton card for loading state
+const SkeletonItem = () => {
+  return (
+    <div className='item skeleton'>
+      <div className="inner-container">
+        <div className="skeleton-image"></div>
+      </div>
+      <div className='skeleton-text skeleton-product-name'></div>
+      <div className="skeleton-text skeleton-discount"></div>
+      <div className='item-prices'>
+        <div className="skeleton-text skeleton-price-old"></div>
+        <div className="skeleton-text skeleton-price-new"></div>
+      </div>
+    </div>
+  );
+};
 
 const Item = (props) => {
   useIntersectionObserver('.animate-down', 'show');
+
+  // Show skeleton if data is not loaded
+  if (!props.name || !props.image || !props.new_price || !props.old_price) {
+    return <SkeletonItem />;
+  }
+
   const discountPercentage = Math.round(((props.old_price - props.new_price) / props.old_price) * 100);
+
   return (
     <div className='item animate-down'>
       <div className="inner-container">
-        <Link to={`/product/${props._id}`}><img onClick={window.scrollTo(0, 0)} className='image[0]' id='image' src={props?.image[0]} /></Link>
-        <Link to={`/product/${props._id}`}><img onClick={window.scrollTo(0, 0)} className='image[1]' id='image2' src={props?.image[1]} /></Link>
-        <Link to={`/product/${props._id}`}><img onClick={window.scrollTo(0, 0)} className='image[2]' id='image3' src={props?.image[2]} /></Link>
-        <Link to={`/product/${props._id}`}><img onClick={window.scrollTo(0, 0)} className='image[3]' id='image4' src={props?.image[3]} /></Link>
+        <Link to={`/product/${props._id}`}>
+          <img onClick={() => window.scrollTo(0, 0)} className='image[0]' id='image1' src={props?.image[0]} alt="Product" />
+        </Link>
+        <Link to={`/product/${props._id}`}>
+          <img onClick={() => window.scrollTo(0, 0)} className='image[1]' id='image2' src={props?.image[1]} alt="Product" />
+        </Link>
+        <Link to={`/product/${props._id}`}>
+          <img onClick={() => window.scrollTo(0, 0)} className='image[2]' id='image3' src={props?.image[2]} alt="Product" />
+        </Link>
+        <Link to={`/product/${props._id}`}>
+          <img onClick={() => window.scrollTo(0, 0)} className='image[3]' id='image4' src={props?.image[3]} alt="Product" />
+        </Link>
       </div>
       <p className='product-name'>{props.name}</p>
       <div className="item-discount"><b>Discount {discountPercentage}%</b></div>
@@ -25,7 +57,7 @@ const Item = (props) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
